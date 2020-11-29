1f3c21cd57ded14f402d2ebfc5657390506fce27
TommiKall <tommi.kalliomaki98@gmail.com>
2020-11-29 20:58:21 +0200

    muutokset

---
+++ src/Btn.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+
+//nappulat
+function Btn(props) {
+  return (
+    <div>
+        {(props.status===0)?
+        <button className="stopwatch" onClick={props.start}>Start</button> : ""
+        }   
+
+
+        {(props.status===1)?
+        <div>
+            <button className="stopwatch" onClick={props.stop}>Pause</button>
+            <button className="stopwatch" onClick={props.reset}>Reset</button>
+        </div> : ""
+        }
+            
+        {(props.status===2)?
+        <div>
+            <button className="stopwatch" onClick={props.resume}>Start</button>
+            <button className="stopwatch" onClick={props.reset}>Reset</button>
+            <button className="stopwatch" onClick={props.save}>Save</button>
+        </div> : ""
+        }
+
+
+
+    </div> 
+     
+  );
+}
+
+export default Btn;
---
+++ src/Saver.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Saver(props) {
+    return (
+      <div>
+        <p> moroo </p>
+
+      </div>
+       
+      
+      );
+  }
+
+
+
+
+export default Saver;
